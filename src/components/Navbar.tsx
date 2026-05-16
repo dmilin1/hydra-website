aa3f00ac88f41096c0f1d85c370ec5912c4221c9
dmilin1 <dmilinovichiii@gmail.com>
2026-05-15 19:55:35 -0700

    add github link to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,21 +2,24 @@ import downloadButton from '../assets/download-button.svg';
 
 export default function Navbar() {
     return (
-        <div className='flex flex-col sm:flex-row justify-between items-center gap-4 h-16 bg-white text-black mb-16'>
-            <div className='flex items-center justify-evenly gap-4 w-full sm:w-auto text-md'>
-                <a href='/'>Home</a>
-                <a href='https://www.reddit.com/r/HydraClient/'>/r/HydraClient</a>
-                <a href='/privacy'>Privacy</a>
-                <a href='/support'>Support</a>
+        <nav className='w-full bg-white text-black border-t border-gray-200 mt-12'>
+            <div className='max-w-[1200px] mx-auto flex flex-col sm:flex-row sm:items-center sm:justify-between gap-5 px-6 py-5'>
+                <ul className='flex flex-wrap items-center justify-center gap-x-5 gap-y-2 text-sm sm:text-base list-none p-0 m-0'>
+                    <li><a href='/'>Home</a></li>
+                    <li><a href='https://www.reddit.com/r/HydraClient/'>/r/HydraClient</a></li>
+                    <li><a href='https://github.com/dmilin1/hydra'>GitHub</a></li>
+                    <li><a href='/privacy'>Privacy</a></li>
+                    <li><a href='/support'>Support</a></li>
+                </ul>
+                <div className='flex items-center justify-center gap-4 shrink-0'>
+                    <a href='https://apps.apple.com/us/app/hydra-for-reddit/id6478089063'>
+                        <img className='h-8' src={downloadButton} alt='Download on the App Store' />
+                    </a>
+                    <a href='https://discord.gg/ypaD4KYJ3R'>
+                        <img className='h-8' src='https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/653714c1c2d8d50382c7df8a_636e0b5061df29d55a92d945_full_logo_blurple_RGB.svg' alt='Join our Discord' />
+                    </a>
+                </div>
             </div>
-            <div className='flex gap-4 justify-center'>
-                <a href='https://apps.apple.com/us/app/hydra-for-reddit/id6478089063'>
-                    <img className='h-8' src={downloadButton} />
-                </a>
-                <a href='https://discord.gg/ypaD4KYJ3R'>
-                    <img className='h-8' src='https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/653714c1c2d8d50382c7df8a_636e0b5061df29d55a92d945_full_logo_blurple_RGB.svg' />
-                </a>
-            </div>
-        </div>
+        </nav>
     );
 }
